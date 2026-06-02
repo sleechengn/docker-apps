ec4c989a339815f6660ae50c54d28f6144435f70
alicia <sleechengn@live.cn>
2026-06-02 02:43:46 +0000

    gitea

---
+++ gitea/boot.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+if [ ! -e "$(dirname $0)/database" ]; then
+	mkdir $(dirname $0)/database
+fi
+
+if [ ! -e "$(dirname $0)/gitea" ]; then
+	mkdir $(dirname $0)/gitea
+fi
+
+if [ "$(which podman)" ]; then
+	podman compose -f $(dirname $0)/docker-compose.yml up -d
+else
+	docker compose -f $(dirname $0)/docker-compose.yml up -d
+fi
