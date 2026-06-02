64062290b78196b4f46d83d3ec57d0191d201ee1
alicia <sleechengn@live.cn>
2026-06-02 02:56:37 +0000

    update

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ gitea 源码管理私人部署
 
 pansou 网盘搜索
 
+registry 私有容器仓库
---
+++ registry/boot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+if [ ! -e "$(dirname $0)/registry" ]; then
+	mkdir $(dirname $0)/registry
+fi
+
+
+if [ "$(which podman)" ]; then
+	podman compose -f $(dirname $0)/docker-compose.yml up -d
+else
+	docker compose -f $(dirname $0)/docker-compose.yml up -d
+fi
